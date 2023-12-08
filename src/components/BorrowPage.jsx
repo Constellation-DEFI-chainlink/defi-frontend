71d5ac8e24292af35b6bfddd2f5e69ab7a17f17e
Aleksander Wojcik <aleksander.w1992@gmail.com>
2023-12-09 00:42:08 +0100

    BorrowPage.jsx

--- src/components/BorrowPage.jsx
+++ src/components/BorrowPage.jsx
@@ -1,83 +1,53 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import DefiService from '../service/DefiService';
-import { useEffect } from 'react';
+
 const BorrowPage = () => {
-  const [activeTab, setActiveTab] = useState('borrow');
   const [loanAmount, setLoanAmount] = useState('');
-  const [loanDuration, setLoanDuration] = useState('');
-  const [lendAmount, setLendAmount] = useState('');
-  const [message, setMessage] = useState('');
-  const [estimatedInterestRate, setEstimatedInterestRate] = useState('');
-  const [expectedReturn, setExpectedReturn] = useState('');
-  const [weather, setWeather] = useState({ temperature: '', windSpeed: '' });
+  const [collateralDetails, setCollateralDetails] = useState(null);
   const [location, setLocation] = useState({ latitude: '', longitude: '' });
+  const [message, setMessage] = useState('');
+  const [weather, setWeather] = useState({ temperature: '', windSpeed: '' });
 
-
-  const calculateInterestRate = () => {
-    DefiService.calculateEstimatedInterestRateForLend()
-    .then(response => {
-      showToast(response);
-      setEstimatedInterestRate(response);
-    })
-    .catch(error => showToast(error));
+  const showToast = (msg) => {
+    console.log(msg); // For debugging
+    setMessage(msg);
+    setTimeout(() => setMessage(''), 3000); // Clear message after 3 seconds
   };
 
-  const calculateReturn = () => {
-    DefiService.calculateExpectedReturnForLend()
-    .then(response => {
-      showToast(response);
-      setExpectedReturn(response);
-    })
-    .catch(error => showToast(error));
-  };
+  const handleCalculateCollateral = async () => {
+    if (!loanAmount || parseFloat(loanAmount) <= 0) {
+      showToast('Please enter a valid loan amount greater than 0.');
+      return;
+    }
 
-  const handleRequestLoan = () => {
-    if (loanAmount > 0 && loanDuration > 0) {
-      DefiService.createLoanRequest(loanAmount, loanDuration)
-      .then(response => {
-        showToast(response);
-        // Reset the form and related states
-        setLoanAmount('');
-        setLoanDuration('');
-        setEstimatedInterestRate('');
-      })
-      .catch(error => showToast(error));
+    const latitude = parseFloat(location.latitude);
+    const longitude = parseFloat(location.longitude);
+    if (isNaN(latitude) || isNaN(longitude)) {
+      showToast('Invalid location data. Please enter valid latitude and longitude.');
+      return;
+    }
+
+    try {
+      await fetchWeatherData(parseFloat(location.latitude), parseFloat(location.longitude));
+      const interestRateResponse = await DefiService.getCollateralAmount(loanAmount, weather.temperature);
+      setCollateralDetails(interestRateResponse);
+      showToast('Collateral amount calculated based on current weather data.');
+    } catch (error) {
+      showToast(error);
     }
   };
 
-  const handleLendAssets = () => {
-    if (lendAmount > 0) {
-      DefiService.deposit(lendAmount)
-      .then(response => {
-        showToast(response);
-        // Reset the form and related states
-        setLendAmount('');
-        setExpectedReturn('');
-      })
-      .catch(error => showToast(error));
+  const handleAcceptLoanOffer = async () => {
+    try {
+      const acceptResponse = await DefiService.acceptLoanOffer(collateralDetails.collateralAmount);
+      showToast(acceptResponse);
+      setLoanAmount('');
+      setCollateralDetails(null);
+    } catch (error) {
+      showToast(error);
     }
   };
 
-  const onLoanAmountChange = (e) => {
-    setLoanAmount(e.target.value);
-    if (estimatedInterestRate) {
-      setEstimatedInterestRate('');
-    }
-  };
-
-  const onLoanDurationChange = (e) => {
-    setLoanDuration(e.target.value);
-    if (estimatedInterestRate) {
-      setEstimatedInterestRate('');
-    }
-  };
-
-  const onLendAmountChange = (e) => {
-    setLendAmount(e.target.value);
-    if (expectedReturn) {
-      setExpectedReturn('');
-    }
-  };
   const fetchWeatherData = async (latitude, longitude) => {
     const response = await fetch(
         `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&current_weather=true`
@@ -89,6 +59,7 @@ const BorrowPage = () => {
       windSpeed: currentWeather.windspeed
     });
   };
+
   const validateAndSetLocation = (lat, lon) => {
     const parsedLat = parseFloat(lat);
     const parsedLon = parseFloat(lon);
@@ -96,38 +67,17 @@ const BorrowPage = () => {
     const isLongitudeValid = !isNaN(parsedLon) && parsedLon >= -180 && parsedLon <= 180;
 
     if (isLatitudeValid && isLongitudeValid) {
-      setLocation({ latitude: parsedLat, longitude: parsedLon });
+      setLocation({ latitude: ""+parsedLat, longitude: ""+parsedLon });
       fetchWeatherData(parsedLat, parsedLon);
     } else {
       showToast('Invalid latitude or longitude. Please enter valid numbers.');
     }
   };
 
-  const showToast = (msg) => {
-    console.log(msg);
-    setMessage(msg);
-    setTimeout(() => setMessage(''), 3000); // Clear message after 3 seconds
-  };
-
-  // Attempt to fetch the user's current location on component mount
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        fetchWeatherData(position.coords.latitude, position.coords.longitude);
-        setLocation({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude
-        });
-      }, (error) => {
-        console.error(error);
-      });
-    }
-  }, []);
-
   return (
-      <div className="container mx-auto p-4 text-white"> {/* Adjusted for white text */}
+      <div className="container mx-auto p-4 text-white">
         <h1 className="text-2xl font-bold mb-4">Weather-Indexed Loan Marketplace</h1>
-        <p>Welcome to the Weather-Indexed Loan Marketplace, where your loan terms adapt to real-time weather conditions.</p>
+        <p>Welcome to our platform where loan terms adapt to real-time weather conditions.</p>
         {message && <div className="toast-message">{message}</div>}
 
         {/* Weather Panel */}
@@ -166,89 +116,41 @@ const BorrowPage = () => {
           </div>
         </div>
 
-        {/* Process Description */}
-        <div className="mb-6">
-          <p>Select whether you want to request a loan or lend your assets, then calculate the estimated interest rate or expected return before proceeding.</p>
-        </div>
+        {/* Loan Request Section */}
+        <div className="loan-request-section my-6">
+          <label htmlFor="loan-amount" className="block my-2">Loan Amount</label>
+          <input
+              id="loan-amount"
+              name="loanAmount"
+              type="number"
+              placeholder="Enter loan amount in Solc Coin"
+              className="border p-2 rounded w-full text-black"
+              min={0}
+              value={loanAmount}
+              onChange={(e) => setLoanAmount(e.target.value)}
+          />
 
-        <div className="mb-4 flex border-b">
           <button
-              className={`mr-4 pb-2 w-1/2 ${activeTab === 'borrow' ? 'bg-[#000300] text-[#00df9a]' : 'text-gray-500'}`}
-              onClick={() => setActiveTab('borrow')}
+              className={`bg-blue-500 text-white rounded p-2 hover:bg-blue-700 my-2 ${!loanAmount ? 'opacity-50 cursor-not-allowed' : ''}`}
+              onClick={handleCalculateCollateral}
+              disabled={!loanAmount}
           >
-            Request a Loan
+            Calculate Collateral
           </button>
-          <button
-              className={`pb-2 w-1/2 ${activeTab === 'lend' ? 'bg-[#000300] text-[#00df9a]' : 'text-gray-500'}`}
-              onClick={() => setActiveTab('lend')}
-          >
-            Lend Your Assets
-          </button>
+
+          {collateralDetails && (
+              <div className="collateral-details">
+                <p>Calculated Collateral Amount: {collateralDetails.collateralAmount} ETH</p>
+                <p>Interest Rate: {collateralDetails.interestRate}%</p>
+                <button
+                    className={`bg-green-500 text-white rounded p-2 hover:bg-green-700 my-2`}
+                    onClick={handleAcceptLoanOffer}
+                >
+                  Accept Loan and Pay Collateral
+                </button>
+              </div>
+          )}
         </div>
-
-        {/* Borrow Tab */}
-        {activeTab === 'borrow' && (
-            <div>
-              <label htmlFor="loan-amount" className="block my-2">Loan Amount</label>
-              <input id="loan-amount" name="loanAmount" type="number"
-                     placeholder="Loan Amount" className="border p-2 rounded w-full text-black"
-                     min={0}
-                     value={loanAmount}
-                     onChange={onLoanAmountChange}
-              />
-              <label htmlFor="loan-duration" className="block my-2">Loan Duration (in days)</label>
-              <input id="loan-duration" name="loanDuration" type="number"
-                     placeholder="Loan Duration" className="border p-2 rounded w-full text-black"
-                     min={0}
-                     value={loanDuration}
-                     onChange={onLoanDurationChange}
-              />
-              <button id="calculate-interest-rate"
-                      className={`bg-green-500 text-white rounded p-2 hover:bg-green-700 my-2 ${!(loanAmount >0 && loanDuration > 0) && 'opacity-50 cursor-not-allowed'}`}
-                      onClick={calculateInterestRate}
-                      disabled={!(loanAmount >0 && loanDuration > 0)}
-              >
-                Calculate Estimated Interest Rate
-              </button>
-              <p>{estimatedInterestRate}</p>
-              <button id="request-loan"
-                      className={`bg-blue-500 text-white rounded p-2 hover:bg-blue-700 my-2 ${!estimatedInterestRate ? 'opacity-50 cursor-not-allowed' : ''}`}
-                      onClick={handleRequestLoan}
-                      disabled={!estimatedInterestRate}
-              >
-                Request Loan
-              </button>
-            </div>
-        )}
-
-        {/* Lend Tab */}
-        {activeTab === 'lend' && (
-            <div>
-              <label htmlFor="lend-amount" className="block my-2">Amount to Lend</label>
-              <input id="lend-amount" name="lendAmount" type="number"
-                     placeholder="Amount to Lend" className="border p-2 rounded w-full text-black"
-                     min={0}
-                     value={lendAmount}
-                     onChange={onLendAmountChange}
-              />
-              <button id="calculate-return"
-                      className={`bg-green-500 text-white rounded p-2 hover:bg-green-700 my-2 ${!(lendAmount > 0) && 'opacity-50 cursor-not-allowed'}`}
-                      onClick={calculateReturn}
-                      disabled={!(lendAmount > 0)}
-
-              >
-                Calculate Expected Return
-              </button>
-              <p>{expectedReturn}</p>
-              <button id="lend-assets"
-                      className={`bg-blue-500 text-white rounded p-2 hover:bg-blue-700 my-2 ${!expectedReturn ? 'opacity-50 cursor-not-allowed' : ''}`}
-                      onClick={handleLendAssets}
-                      disabled={!expectedReturn}
-              >
-                Lend Assets
-              </button>
-            </div>
-        )}
       </div>
   );
 };
--- src/service/DefiService.js
+++ src/service/DefiService.js
@@ -1,60 +1,83 @@
-const mockSuccessOrFail = () => {
-  // Randomly resolve or reject - will be replaced by smart contract communication
-  return Math.random() > 0.35;
+const mockSuccessOrFail = () => Math.random() > 0.35;
+const mockRandomPercentage = () => (Math.random() * 10).toFixed(3);
+
+const getCollateralAmount = (loanAmount, temperature) => {
+  const ETH_PRICE = 2300; // USD
+  const MIN_INTEREST_RATE = 0.03; // 3%
+  const MAX_INTEREST_RATE = 0.50; // 50%
+  const MIN_TEMP = -30; // Minimum expected temperature
+  const MAX_TEMP = 40; // Maximum expected temperature
+
+  // Calculate interest rate based on temperature
+  // This is a linear mapping from the temperature to the interest rate
+  let interestRate = MIN_INTEREST_RATE + (MAX_TEMP - temperature) * (MAX_INTEREST_RATE - MIN_INTEREST_RATE) / (MAX_TEMP - MIN_TEMP);
+
+  // Ensure interest rate is within the bounds of 3% to 50%
+  interestRate = Math.max(MIN_INTEREST_RATE, Math.min(interestRate, MAX_INTEREST_RATE));
+
+  // Calculate the amount of ETH needed for the loan amount at the current ETH price
+  const ethNeeded = loanAmount / ETH_PRICE;
+
+  // Calculate the collateral amount in ETH
+  const collateralAmount = ethNeeded * (1 + interestRate);
+
+  // Return a Promise that resolves with the collateral amount and interest rate
+  // The use of Promise here is to mimic the asynchronous behavior of fetching data
+  return new Promise((resolve, reject) => {
+    if (Math.random() > 0.35) { // Mocking a success or fail response
+      resolve({
+        collateralAmount: collateralAmount,
+        interestRate: interestRate * 100 // Convert to percentage for the user
+      });
+    } else {
+      reject('Failed to calculate collateral amount.');
+    }
+  });
 };
 
-const mockRandomPercentage = () => (Math.random() * 10).toFixed(3) + '%';
 
-const calculateEstimatedInterestRateForLend = () => {
+const acceptLoanOffer = (collateralAmount) => {
+  // Mocking the loan acceptance and solstice coin minting process
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (Math.random() > 0.5) {
-        resolve(`Estimated Interest Rate per Year: ${mockRandomPercentage()}`);
+      if (mockSuccessOrFail()) {
+        resolve(`Loan offer accepted. ${collateralAmount} in collateral locked.`);
       } else {
-        reject('Failed to calculate interest rate.');
+        reject('Failed to accept loan offer.');
       }
     }, 1000);
   });
 };
 
-const calculateExpectedReturnForLend = () => {
+const withdrawLoan = (loanAmount) => {
+  // Mocking the loan withdrawal process
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (Math.random() > 0.5) {
-        resolve(`Expected Return: ${mockRandomPercentage()}`);
+      if (mockSuccessOrFail()) {
+        resolve(`Loan amount of ${loanAmount} withdrawn successfully.`);
       } else {
-        reject('Failed to calculate expected return.');
+        reject('Failed to withdraw loan.');
       }
     }, 1000);
   });
 };
-const createLoanRequest = (amount, duration) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (mockSuccessOrFail()) {
-        resolve(`Loan request for ${amount} with duration ${duration} created successfully.`);
-      } else {
-        reject('Failed to create loan request.');
-      }
-    }, 1000); // Simulating network delay
-  });
-};
 
-const deposit = (amount) => {
+const payLoan = (loanId, repaymentAmount) => {
+  // Mocking the loan repayment process
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (mockSuccessOrFail()) {
-        resolve(`Successfully lent ${amount}.`);
+        resolve(`Loan ${loanId} repaid with amount ${repaymentAmount}.`);
       } else {
-        reject('Failed to lend assets.');
+        reject('Failed to repay loan.');
       }
-    }, 1000); // Simulating network delay
+    }, 1000);
   });
 };
 
 export default {
-  createLoanRequest,
-  deposit,
-  calculateEstimatedInterestRateForLend,
-  calculateExpectedReturnForLend
+  getCollateralAmount,
+  acceptLoanOffer,
+  withdrawLoan,
+  payLoan
 };
